81e8f178b69c3ed3b9c8aec8384b61520a2a4c36
Znak Zorro <ZnakZorro@users.noreply.github.com>
2017-03-23 17:50:10 +0100

    Update xxxxxxx.node.js

--- xxxxxxx.node.js
+++ xxxxxxx.node.js
@@ -9,15 +9,27 @@ module.exports = function(RED) {
 		
         var node = this;
         this.on('input', function(msg) {
-					this.status({fill:"red",shape:"ring",text:"Waiting for xxxxxxx"});
-		    	let that = this;
-             /*-----------------------*/
-            let context = this.context();
-            let xxxxxxxcount = context.get('xxxxxxxcount') || 0;
-            xxxxxxxcount += 1;
-          msg.payload = 1;
-          let msg1={"payload":2};
-          node.send([msg,msg1]);
+		this.status({fill:"red",shape:"ring",text:"Waiting for xxxxxxx"});
+		let that = this;
+			//console.log(this.interval,this.bariera);
+			//console.log(node.interval,node.bariera);
+
+			 /*-----------------------*/
+			let context = this.context();
+			let xxxxxxxcount = context.get('xxxxxxxcount') || 0;
+			xxxxxxxcount += 1;
+			context.set('xxxxxxxcount',xxxxxxxcount);
+
+			var d = new Date();
+			var Time = d.getTime();
+			var lacalTime = d.toLocaleString();
+
+			msg.payload = xxxxxxxcount;
+			var msg2={"payload":Time};				
+			var msg3={"payload":lacalTime};
+			var msg4={"payload":{"lacalTime":lacalTime,"xxxxxxxcount":xxxxxxxcount,"Time":Time}};
+			/*-----------------------*/
+			node.send([msg,msg2,msg3,msg4]);	
         });
     }
     RED.nodes.registerType("xxxxxxx",xxxxxxxNode);
